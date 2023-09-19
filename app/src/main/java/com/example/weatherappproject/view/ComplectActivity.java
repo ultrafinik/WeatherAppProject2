3ed62d49493fa301d0cd0eb73ee9f11517a6b863
Andrey <rakdav@inbox.ru>
2023-09-19 03:40:51 +0200

    ComplectActivity1

--- app/src/main/java/com/example/weatherappproject/view/ComplectActivity.java
+++ app/src/main/java/com/example/weatherappproject/view/ComplectActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.weatherappproject.R;
@@ -12,20 +13,18 @@ import com.example.weatherappproject.model.Complect;
 import com.squareup.picasso.Picasso;
 
 public class ComplectActivity extends AppCompatActivity {
-    private ImageView footwearImage,headerImage, outwearImage,pantsImage,shirtImage;
-    private Button buttonFootwear;
+    private ImageButton headerImage, outwearImage,pantsImage,shirtImage, buttonFootwear;
     private String source;
     private Complect complect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_complect);
-        footwearImage = findViewById(R.id.footwear_image);
+        buttonFootwear = findViewById(R.id.footwear_image);
         headerImage = findViewById(R.id.headgear_image);
         outwearImage = findViewById(R.id.outerwear_image);
         pantsImage = findViewById(R.id.pants_image);
         shirtImage = findViewById(R.id.shirt_image);
-        buttonFootwear=findViewById(R.id.button_footwear);
         Bundle arg=getIntent().getExtras();
         if(arg!=null)
         {
@@ -34,23 +33,18 @@ public class ComplectActivity extends AppCompatActivity {
             if (complect != null)
             {
                 if (!complect.getFootwear().equals("")) {
-                    footwearImage.setVisibility(View.VISIBLE);
-                    Picasso.get().load(complect.getFootwear()).into(footwearImage);
+                    Picasso.get().load(complect.getFootwear()).into(buttonFootwear);
                 }
                 if (!complect.getHeadgear().equals("")) {
-                    headerImage.setVisibility(View.VISIBLE);
                     Picasso.get().load(complect.getHeadgear()).into(headerImage);
                 }
                 if (!complect.getOuterwear().equals("")) {
-                    outwearImage.setVisibility(View.VISIBLE);
                     Picasso.get().load(complect.getOuterwear()).into(outwearImage);
                 }
                 if (!complect.getPants().equals("")) {
-                    pantsImage.setVisibility(View.VISIBLE);
                     Picasso.get().load(complect.getPants()).into(pantsImage);
                 }
                 if (!complect.getFootwear().equals("")) {
-                    shirtImage.setVisibility(View.VISIBLE);
                     Picasso.get().load(complect.getShirt()).into(shirtImage);
                 }
             }
